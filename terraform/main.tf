295c30c28eb4bc874294a48d7634b231b9614a95
Thoriet <hanzman@hanzman.net>
2025-12-17 09:36:22 +0100

    Format Terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_version = ">= 1.5.0"
 
   required_providers {
-  random = {
-    source  = "hashicorp/random"
-    version = "~> 3.6"
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
   }
-}
 
   cloud {
     organization = "THORIET-CLOUD"
